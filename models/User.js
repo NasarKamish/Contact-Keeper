56e798f5ce72bd85a9107305d5f0d033b069e16f
Nasar <NasarKamish@gmail.com>
2021-09-29 13:26:09 +0200

    Added new file

---
+++ models/User.js
@@ -0,0 +1,26 @@
+// // // // // // VARIABLES // // // // // //
+
+const mongoose = require("mongoose");
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// // // // // // EXPORTING MODULE // // // // // //
+
+module.exports = mongoose.model("user", UserSchema);
