36c1db70953906b6d97af30575f1f83d8f281dd4
Nasar <NasarKamish@gmail.com>
2021-09-30 09:18:20 +0200

    Updated file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 // // // // // // VARIABLES // // // // // //
 
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 const User = require("../models/User");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 // // // // // // EXPORTING ROUTER // // // // // //
 
@@ -21,11 +22,37 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        res.status(404).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(543).send("Server Error");
+    }
   }
 );
