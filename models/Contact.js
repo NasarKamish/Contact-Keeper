001d2438df9a48a685a49e3210a0bf3e3a26f8b3
Nasar <NasarKamish@gmail.com>
2021-10-01 09:00:04 +0200

    Updated file

---
+++ models/Contact.js
@@ -0,0 +1,32 @@
+// // // // // // VARIABLES // // // // // //
+
+const mongoose = require("mongoose");
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// // // // // // EXPORTING MODULE // // // // // //
+
+module.exports = mongoose.model("contact", ContactSchema);
