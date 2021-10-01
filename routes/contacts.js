72a3efad193f37d90eaf35fa02e7e221b9f2e9b7
Nasar <NasarKamish@gmail.com>
2021-10-01 12:06:46 +0200

    Updated file

--- routes/contacts.js
+++ routes/contacts.js
@@ -57,12 +57,50 @@ router.post(
 
 // // // // // // PUT METHOLDS // // // // // //
 
-router.put("/", (req, res) => {
-  res.send("Update Contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: "Contact not found" });
+    }
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(404).json({ msg: "Not authorized" });
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 // // // // // // DELETE METHOLDS // // // // // //
 
-router.delete("/", (req, res) => {
-  res.send("Delete Contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: "Contact not found" });
+    }
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(404).json({ msg: "Not authorized" });
+    }
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Contact removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
